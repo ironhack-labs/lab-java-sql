6b99199734e6e65e07bcb785eb075aa887aeaa2b
katalinjordansgmail <115991775+katalinjordansgmail@users.noreply.github.com>
2024-03-02 16:08:23 +0100

    Update AirlineDatabase.sql
    Added creation of database and linking each table to the database

--- AirlineDatabase.sql
+++ AirlineDatabase.sql
@@ -1,11 +1,13 @@
-CREATE TABLE Aircrafts (
+CREATE SCHEMA Airlines;
+
+CREATE TABLE Airlines.Aircrafts (
     ID int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
 	Seats int NOT NULL,
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Customers (
+CREATE TABLE Airlines.Customers (
     ID int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
 	Status varchar(6) NOT NULL,
@@ -13,7 +15,7 @@ CREATE TABLE Customers (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Flights (
+CREATE TABLE Airlines.Flights (
     ID int NOT NULL AUTO_INCREMENT,
     AircraftID int NOT NULL,
     FlightNumber varchar(5) NOT NULL,
@@ -22,7 +24,7 @@ CREATE TABLE Flights (
 	FOREIGN KEY (AircraftID) REFERENCES Aircrafts(ID)
 );
 
-CREATE TABLE Tickets (
+CREATE TABLE Airlines.Tickets (
     ID int NOT NULL AUTO_INCREMENT,
     CustomerID int NOT NULL,
     FlightID int NOT NULL,
@@ -31,12 +33,12 @@ CREATE TABLE Tickets (
 	FOREIGN KEY (FlightID) REFERENCES Flights(ID)
 );
 
-INSERT INTO Aircrafts (Name, Seats) VALUES
+INSERT INTO Airlines.Aircrafts (Name, Seats) VALUES
 	('Boeing 747', 400),
 	('Airbus A330', 236),	
     ('Boeing 777', 264);
 
-INSERT INTO Customers (Name, Status, TotalMileage) VALUES
+INSERT INTO Airlines.Customers (Name, Status, TotalMileage) VALUES
     ('Agustine Riviera', 'Silver', 115235),
 	('Alaina Sepulvida', 'None', 6008),
 	('Tom Jones', 'Gold', 205767),
@@ -46,14 +48,14 @@ INSERT INTO Customers (Name, Status, TotalMileage) VALUES
 	('Jennifer Cortez', 'Gold', 300582),
 	('Christian Janco', 'Silver', 14642);
     
-INSERT INTO Flights (AircraftID, FlightNumber, Mileage) VALUES
+INSERT INTO Airlines.Flights (AircraftID, FlightNumber, Mileage) VALUES
 	(1, 'DL143', 135),
 	(2, 'DL122', 4370),
 	(3, 'DL53', 2078),
 	(3, 'DL222', 1765),
 	(1, 'DL37', 531);
     
-INSERT INTO Tickets (CustomerID, FlightID) VALUES
+INSERT INTO Airlines.Tickets (CustomerID, FlightID) VALUES
 	(1, 1),
 	(1, 2),
 	(2, 2),
