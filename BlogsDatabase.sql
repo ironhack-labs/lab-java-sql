841eecf531e85ab1a4f88278163330d98bc98507
katalinjordansgmail <115991775+katalinjordansgmail@users.noreply.github.com>
2024-03-02 16:16:30 +0100

    Update BlogsDatabase.sql

--- BlogsDatabase.sql
+++ BlogsDatabase.sql
@@ -1,12 +1,13 @@
-CREATE SCHEMA Blogs;
+CREATE DATABASE Blogs;
+USE Blogs;
 
-CREATE TABLE Blogs.Authors (
+CREATE TABLE Authors (
     ID int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Blogs.Blogs (
+CREATE TABLE Posts (
     ID int NOT NULL AUTO_INCREMENT,
     AuthorID int NOT NULL,
     Title varchar(255) NOT NULL,
@@ -16,12 +17,12 @@ CREATE TABLE Blogs.Blogs (
 	FOREIGN KEY (AuthorID) REFERENCES Authors(ID)
 );
 
-INSERT INTO Blogs.Authors (Name) VALUES
+INSERT INTO Authors (Name) VALUES
     ('Maria Charlotte'),
     ('Juan Perez'),
     ('Gemma Alcocer');
 
-INSERT INTO Blogs.Blogs (AuthorID, Title, WordCount, Views) VALUES
+INSERT INTO Posts (AuthorID, Title, WordCount, Views) VALUES
     (1, 'Best Paint Colors', 814,14),
     (2, 'Small Space Decorating Tips', 1146, 221),
     (1, 'Hot Accessories', 986, 105),
