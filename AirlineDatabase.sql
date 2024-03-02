323df19b8124a316af69a3dd894951c38272b879
katalinjordansgmail <115991775+katalinjordansgmail@users.noreply.github.com>
2024-03-02 17:09:33 +0100

    Update AirlineDatabase.sql

--- AirlineDatabase.sql
+++ AirlineDatabase.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE Airlines;
 USE Airlines;
 
-CREATE TABLE Aircrafts (
+CREATE TABLE Aircraft (
     ID int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
 	Seats int NOT NULL,
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Customers (
+CREATE TABLE Customer (
     ID int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
 	Status varchar(6) NOT NULL,
@@ -16,30 +16,30 @@ CREATE TABLE Customers (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Flights (
+CREATE TABLE Flight (
     ID int NOT NULL AUTO_INCREMENT,
     AircraftID int NOT NULL,
     FlightNumber varchar(5) NOT NULL,
     Mileage int NOT NULL,
     PRIMARY KEY (ID),
-	FOREIGN KEY (AircraftID) REFERENCES Aircrafts(ID)
+	FOREIGN KEY (AircraftID) REFERENCES Aircraft(ID)
 );
 
-CREATE TABLE Tickets (
+CREATE TABLE FlightBooking (
     ID int NOT NULL AUTO_INCREMENT,
     CustomerID int NOT NULL,
     FlightID int NOT NULL,
     PRIMARY KEY (ID),
-	FOREIGN KEY (CustomerID) REFERENCES Customers(ID),
-	FOREIGN KEY (FlightID) REFERENCES Flights(ID)
+	FOREIGN KEY (CustomerID) REFERENCES Customer(ID),
+	FOREIGN KEY (FlightID) REFERENCES Flight(ID)
 );
 
-INSERT INTO Aircrafts (Name, Seats) VALUES
+INSERT INTO Aircraft (Name, Seats) VALUES
 	('Boeing 747', 400),
 	('Airbus A330', 236),	
     ('Boeing 777', 264);
 
-INSERT INTO Customers (Name, Status, TotalMileage) VALUES
+INSERT INTO Customer (Name, Status, TotalMileage) VALUES
     ('Agustine Riviera', 'Silver', 115235),
 	('Alaina Sepulvida', 'None', 6008),
 	('Tom Jones', 'Gold', 205767),
@@ -49,14 +49,14 @@ INSERT INTO Customers (Name, Status, TotalMileage) VALUES
 	('Jennifer Cortez', 'Gold', 300582),
 	('Christian Janco', 'Silver', 14642);
     
-INSERT INTO Flights (AircraftID, FlightNumber, Mileage) VALUES
+INSERT INTO Flight (AircraftID, FlightNumber, Mileage) VALUES
 	(1, 'DL143', 135),
 	(2, 'DL122', 4370),
 	(3, 'DL53', 2078),
 	(3, 'DL222', 1765),
 	(1, 'DL37', 531);
     
-INSERT INTO Tickets (CustomerID, FlightID) VALUES
+INSERT INTO FlightBooking (CustomerID, FlightID) VALUES
 	(1, 1),
 	(1, 2),
 	(2, 2),
